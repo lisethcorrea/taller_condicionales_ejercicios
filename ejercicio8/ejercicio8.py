a326bfebdf0b0075a7782656e8eeaf17a8abcfb8
futbolero06 <oscarardila@colegioguanenta.edu.co>
2023-03-08 07:16:42 -0500

    se creo el diagrama y el programa

--- ejercicio8/ejercicio8.py
+++ ejercicio8/ejercicio8.py
@@ -1,20 +1,15 @@
 # programa para calcular si un número entero en multiplo de otro entero
 
 # input
-x=int(input("digite el primer numero"))
-y=int(input("digite el segundo numero"))
+x=int(input("digite el primer numero: "))
+y=int(input("digite el segundo numero: "))
 
 # prossecing
 
 if x%y==0:
-    msj= "el numero" ,x, "es multiplo de: " ,y,
+    msj= ("el numero" ,x, "es multiplo de: " ,y,)
 else:
-    msj= "no son multiplos"
-    if y%x==0:
-        msj= "el numero" ,y, "es multiplo de: " ,x,
-    else: 
-        msj= "no son multiplos"
-
+    msj= ("no son multiplos")
 
 # output
 
